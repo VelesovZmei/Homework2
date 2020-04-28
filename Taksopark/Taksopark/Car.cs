264f892be8809d9c3e4eaa7515f7a79badccfc93
Zmei <gar0tarkhan>
2020-04-28 21:01:04 +0300

    Add file

---
+++ Taksopark/Taksopark/Car.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Taksopark
+{
+    public class Car
+    {
+        //protected int maxspeed;
+        //protected int numberofpessenger;
+
+        public string model { get; set; }
+        public string Fuel { get; set; }
+        public double VolumeofEngine { get; set; }
+        public double RacingTo100 { get; set; }
+        public int FuelConsumption { get; set; }
+        public int price { get; set; }
+        public virtual int NumberofPessenger { get ; } //{ return numberofpessenger
+        public virtual int MaxSpeed { get; } //{ return maxspeed; }
+
+
+    }
+    public class Sedan : Car
+    {
+        private readonly int maxspeed;
+        private readonly int numberofpessenger;
+        public override int NumberofPessenger { get { return numberofpessenger; } }
+        public override int MaxSpeed { get { return maxspeed; } }
+        public Sedan()
+        {
+            numberofpessenger = 5;
+            maxspeed = 140;
+        }
+    }
+    public class Minivan : Car
+    {
+        private readonly int maxspeed;
+        private readonly int numberofpessenger;
+        public override int NumberofPessenger { get { return numberofpessenger; } }
+        public override int MaxSpeed { get { return maxspeed; } }
+        public Minivan()
+        {
+            numberofpessenger = 7;
+            maxspeed = 120;
+        }
+    }
+    public class MicroBus : Car
+    {
+        private readonly int maxspeed;
+        private readonly int numberofpessenger;
+        public override int NumberofPessenger { get { return numberofpessenger; } }
+        public override int MaxSpeed { get { return maxspeed; } }
+        public MicroBus()
+        {
+            numberofpessenger = 11;
+            maxspeed = 100;
+        }
+    }
+    public class Taxopark
+    {
+        public List<Car> Cars;
+
+        public void Sort(bool Descending)
+        {
+            IEnumerable<Car> sortedtaksopark;
+            if (Descending)
+               { sortedtaksopark = Cars.OrderByDescending(FC => FC.FuelConsumption); }
+            else
+               { sortedtaksopark = Cars.OrderBy(FC => FC.FuelConsumption); }
+            Cars = sortedtaksopark.ToList();
+        }
+
+        public void GetInfo()
+        {
+            foreach (var item in Cars)
+            {
+                Console.WriteLine($"Модель: {item.model},\t Вместимость, чел: {item.NumberofPessenger},\t Топлива: {item.Fuel},\t Расход топлива на 100 км: {item.FuelConsumption},\t Разгон до 100км/ч, сек: {item.RacingTo100},\t Цена: {item.price} $");
+            }
+                  
+        }
+
+    }
+}
---
+++ Taksopark/Taksopark/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace Taksopark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string CarList;
+
+
+            try
+            {
+                using (StreamReader sr = new StreamReader("E:\\Study\\C#\\Taksopark\\Taksopark\\Sedans.json"))
+                {
+                    CarList=sr.ReadToEnd();
+                }
+
+                var Sedans = JsonSerializer.Deserialize<List<Sedan>>(CarList);
+                using (StreamReader sr = new StreamReader("E:\\Study\\C#\\Taksopark\\Taksopark\\Minivans.json"))
+                {
+                    CarList = sr.ReadToEnd();
+                }
+
+                var Minivans = JsonSerializer.Deserialize<List<Minivan>>(CarList);
+                using (StreamReader sr = new StreamReader("E:\\Study\\C#\\Taksopark\\Taksopark\\Microbuses.json"))
+                {
+                    CarList = sr.ReadToEnd();
+                }
+
+                var Microbuses = JsonSerializer.Deserialize<List<MicroBus>>(CarList);
+
+                Taxopark taksopark = new Taxopark();
+                taksopark.Cars = new List<Car>(13);
+                Random rng = new Random();
+                for (int i = 0; i < 5; i++)
+                {
+                    taksopark.Cars.Add(Sedans[rng.Next(0, 4)]);
+                }
+                for (int i = 0; i < 5; i++)
+                {
+                    taksopark.Cars.Add(Minivans[rng.Next(0, 4)]);
+                }
+                for (int i = 0; i < 3; i++)
+                {
+                    taksopark.Cars.Add(Microbuses[rng.Next(0, 4)]);
+                }
+                taksopark.Sort(true);
+                taksopark.GetInfo();
+
+                var Sum = taksopark.Cars.Sum(p => p.price);
+                Console.WriteLine();
+                Console.WriteLine($"Стоимость машин = {Sum} $");
+
+                while (true)
+                {
+                    Console.WriteLine("Введите минимальную скорость разгона(не менее 5 секунд, не более 19 секунд)");
+                    var min = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Введите максимальную скорость разгона(не менее 6 секунд, не более 20 секунд)");
+                    var max = Convert.ToDouble(Console.ReadLine());
+
+
+                    if (max < min)
+                    {
+                        var swap = max;
+                        max = min;
+                        min = swap;
+                    }
+
+
+                    var car = taksopark.Cars.Where(R100 => R100.RacingTo100 <= max && R100.RacingTo100 >= min).FirstOrDefault();
+                    if (car == null)
+                    {
+                        Console.WriteLine("Данному диапазону транспорт не пренадлежит. Введите ещё раз.");
+                    }
+                    else
+                    {
+                        Console.WriteLine(car.model);
+                        break;
+                    }
+
+                }
+
+
+
+
+            }
+            catch (JsonException e)
+            {
+                Console.WriteLine("JSON не верен");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
